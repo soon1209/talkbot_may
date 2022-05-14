70ab4540d71eba4b923f8a78a2f9088256ec4abf
suniiiiii <soon891209@gmail.com>
2022-05-14 15:29:03 +0900

    Add files via upload

---
+++ conf/config.py
@@ -0,0 +1,46 @@
+#!/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Copyright 2020-present Works Mobile Corp.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+import os
+
+# default language ['en'|'ko'|'ja']
+DEFAULT_LANG = 'ko'
+
+# TimeZone
+TZone = "Asia/Seoul"
+
+# account
+ADMIN_ACCOUNT = "sg@sgapitest"
+DOMAIN_ID = 300018927
+
+# api
+API_ID = "kr1JDmRBYMLdT"
+SERVER_CONSUMER_KEY = "ZRaFHClbmVZwgbrqPtLK"
+
+SERVER_ID = "eaa0e05cb0e94a52b1f800e415d3194c"
+SECRET_KEY_NAME = "private_20220406150850.key"
+
+# LOCAL ADDRESS
+LOCAL_ADDRESS = "https://attendance-bot-learning.herokuapp.com/"
+
+# DB config
+DB_HOST = "ec2-18-215-96-22.compute-1.amazonaws.com"
+DB_NAME = "dbjderbi4bd2u5"
+DB_USER = "vtjuzogcymxilr"
+DB_PORT = "5432"
+DB_PASSWORD = "8ba20f78a897797f3c905a31d7e7aca069904f5279cb1357d4e587206cc5192a"
